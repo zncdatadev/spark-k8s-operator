6028215bcb736dd6568ece40f4c2315b614559ed
whg517 <huagang517@126.com>
2024-03-07 21:23:20 +0800

    build: update bundle run and fix typo (#34)

--- api/v1alpha1/sparkhistoryserver_types.go
+++ api/v1alpha1/sparkhistoryserver_types.go
@@ -28,7 +28,7 @@ import (
 // +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 // +operator-sdk:csv:customresourcedefinitions:displayName="Spark History Server"
-// This annotation provides a hint for OLM which resources are managed by OpenTelemetryCollector kind.
+// This annotation provides a hint for OLM which resources are managed by SparkHistoryServer kind.
 // It's not mandatory to list all resources.
 // https://sdk.operatorframework.io/docs/olm-integration/generation/#csv-fields
 // https://sdk.operatorframework.io/docs/building-operators/golang/references/markers/
