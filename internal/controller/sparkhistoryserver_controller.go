3fb648397fc10959e1e9f7c8e911d2a4ef53ea64
steven <lwpk110@163.com>
2024-03-07 20:45:01 +0800

    feat: add rbac permission to pods and secrets (#33)
    * feat: add rbac permission to pods and secrets
    
    * chore: run generate and manifests

--- internal/controller/sparkhistoryserver_controller.go
+++ internal/controller/sparkhistoryserver_controller.go
@@ -38,10 +38,11 @@ type SparkHistoryServerReconciler struct {
 //+kubebuilder:rbac:groups=spark.zncdata.dev,resources=sparkhistoryservers/finalizers,verbs=update
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=persistentvolumeclaims,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=networking.k8s.io,resources=ingresses,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;
+// +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
